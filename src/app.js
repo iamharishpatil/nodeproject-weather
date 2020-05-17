fcd4bf156c0ccdc9cd28f3aa32f90f615051c95a
Harish <hvpatil060999@gmail.com.com>
2020-05-17 22:55:01 +0530

    setup app for heroku

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const forecast = require('./utils/forecast')
 
 
 const app = express()
+const port = process.env.PORT || 3000
 
 //Define paths for Express congif
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -98,8 +99,8 @@ app.get('*',(req, res)=>{
     })
 })
 
- app.listen(3000,()=>{
-     console.log('Server is up baitch.')
+ app.listen(port,()=>{
+     console.log('Server is up baitch.' + port)
  })
 
  
